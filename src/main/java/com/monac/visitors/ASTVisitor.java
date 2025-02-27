5d5d6daaf92c287a3851c43f2e4186ea91ccc0d4
amar <ustavdica.private@gmail.com>
2025-02-27 01:08:39 +0100

    Clean start

--- src/main/java/com/monac/visitors/ASTVisitor.java
+++ src/main/java/com/monac/visitors/ASTVisitor.java
@@ -1,23 +1,5 @@
 package com.monac.visitors;
 
-import com.monac.parser.ast.nodes.expressions.*;
-import com.monac.parser.ast.nodes.statements.*;
-import com.monac.parser.ast.nodes.structure.FunctionDefinitionNode;
-import com.monac.parser.ast.nodes.structure.TranslationUnitNode;
-import com.monac.parser.ast.nodes.structure.TypeSpecifierNode;
-
 public interface ASTVisitor<R> {
-    R visit(TranslationUnitNode node);
-    R visit(FunctionDefinitionNode node);
-    R visit(TypeSpecifierNode node);
-    R visit(CompoundStatementNode node);
-    R visit(ReturnStatementNode node);
-    R visit(IfStatementNode node);
-    R visit(WhileStatementNode node);
-    R visit(ForStatementNode node);
-    R visit(IdentifierNode node);
-    R visit(IntegerConstantNode node);
-    R visit(AssignmentExpressionNode node);
-    R visit(BinaryExpressionNode node);
-    R visit(UnaryExpressionNode node);
+
 }
--- src/test/java/com/monac/parser/ParserTest.java
+++
@@ -1,9 +0,0 @@
-package com.monac.parser;
-
-public class ParserTest {
-
-
-
-
-
-}
