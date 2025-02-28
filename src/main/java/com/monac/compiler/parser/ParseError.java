1a3a1380c16cf006eeab92143ad7147127437112
amar <ustavdica.private@gmail.com>
2025-02-28 16:28:44 +0100

    clean

--- src/main/java/com/monac/compiler/parser/ParseError.java
+++
@@ -1,5 +0,0 @@
-package com.monac.compiler.parser;
-
-public class ParseError extends RuntimeException {
-
-}
--- src/main/java/com/monac/compiler/visitors/Visitor.java
+++
@@ -1,7 +0,0 @@
-package com.monac.compiler.visitors;
-
-import com.monac.compiler.parser.Node;
-
-public interface Visitor {
-    void visit(Node node);
-}
