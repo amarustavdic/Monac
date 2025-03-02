e92be5012a2de6c05921bd30e3b104b3fbb3b039
amar <ustavdica.private@gmail.com>
2025-03-02 12:02:10 +0100

    parsing integer constant, (with refactored version)

--- src/main/java/com/monac/compiler/Main.java
+++ src/main/java/com/monac/compiler/Main.java
@@ -1,27 +1,29 @@
 package com.monac.compiler;
 
 import com.monac.compiler.lexer.Lexer;
-import com.monac.compiler.lexer.Token;
 import com.monac.compiler.parser.Parser;
 import com.monac.compiler.parser.tree.nodes.ConstantNode;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String input = "234";
+        Scanner sc = new Scanner(System.in);
 
-        Parser parser = new Parser(new Lexer(input));
+        System.out.print(">>> ");
 
-        var ast = parser.parse();
+        String input;
+        while ((input = sc.nextLine()) != null) {
+            Parser parser = new Parser(new Lexer(input));
+            var ast = parser.parse();
+            if (ast instanceof ConstantNode) {
+                System.out.println("yay");
+            }
 
-        if (ast instanceof ConstantNode) {
-            System.out.println("yay");
+            System.out.print(">>> ");
         }
-
-
     }
 }
 
