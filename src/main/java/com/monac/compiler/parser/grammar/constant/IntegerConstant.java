f9d1939263a7b1e93774b701afcfe5d7bd610781
amar <ustavdica.private@gmail.com>
2025-03-03 02:00:26 +0100

    documented IntegerConstant

--- src/main/java/com/monac/compiler/parser/grammar/constant/IntegerConstant.java
+++ src/main/java/com/monac/compiler/parser/grammar/constant/IntegerConstant.java
@@ -6,8 +6,22 @@ import com.monac.compiler.parser.Parser;
 import com.monac.compiler.parser.tree.Node;
 import com.monac.compiler.parser.tree.NodeType;
 
+/**
+ * A utility class for parsing integer constants in the source code.
+ * <p>
+ * This class provides a method to parse integer constant tokens
+ * from the parser and convert them into a corresponding AST (Abstract Syntax Tree) node.
+ * </p>
+ **/
 public final class IntegerConstant {
 
+    /**
+     * Parses an integer constant from the parser if the next token matches an integer constant.
+     *
+     * @param parser The parser instance used to analyze the token stream.
+     * @return A {@link Node} representing the integer constant in the AST,
+     *         or {@code null} if no integer constant is found.
+     */
     public static Node parse(Parser parser) {
         if (parser.match(TokenType.INTEGER_CONSTANT)) {
             Token token = parser.previous();
@@ -15,7 +29,6 @@ public final class IntegerConstant {
             node.setLiteral(Integer.parseInt(token.getLexeme()));
             return node;
         }
-        // TODO: Sync if this happens in integer constant, but later
         return null;
     }
 
