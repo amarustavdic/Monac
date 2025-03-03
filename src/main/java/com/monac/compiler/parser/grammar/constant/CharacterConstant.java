dba0d718d6a912666849da0a9fc1027f25ff6f72
amar <ustavdica.private@gmail.com>
2025-03-03 02:12:07 +0100

    implemented top level class that represents constants, well now only integer as constant is fully implemented

---
+++ src/main/java/com/monac/compiler/parser/grammar/constant/CharacterConstant.java
@@ -0,0 +1,12 @@
+package com.monac.compiler.parser.grammar.constant;
+
+import com.monac.compiler.parser.Parser;
+import com.monac.compiler.parser.tree.Node;
+
+public final class CharacterConstant {
+
+    public static Node parse(Parser parser) {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/monac/compiler/parser/grammar/constant/EnumerationConstant.java
@@ -0,0 +1,12 @@
+package com.monac.compiler.parser.grammar.constant;
+
+import com.monac.compiler.parser.Parser;
+import com.monac.compiler.parser.tree.Node;
+
+public final class EnumerationConstant {
+
+    public static Node parse(Parser parser) {
+        return null;
+    }
+
+}
