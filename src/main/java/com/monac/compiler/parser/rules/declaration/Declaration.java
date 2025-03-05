6adaa9d03eb553db49503f7edec4336405447fc1
amar <ustavdica.private@gmail.com>
2025-03-05 21:14:51 +0100

    refactored declaration

--- src/main/java/com/monac/compiler/parser/rules/declaration/Declaration.java
+++ src/main/java/com/monac/compiler/parser/rules/declaration/Declaration.java
@@ -1,7 +1,9 @@
 package com.monac.compiler.parser.rules.declaration;
 
+import com.monac.compiler.lexer.Token;
 import com.monac.compiler.lexer.TokenType;
 import com.monac.compiler.parser.Parser;
+import com.monac.compiler.parser.ParserException;
 import com.monac.compiler.parser.rules.declarator.InitDeclarator;
 import com.monac.compiler.parser.rules.specifier.DeclarationSpecifier;
 import com.monac.compiler.parser.tree.Node;
@@ -10,38 +12,54 @@ import com.monac.compiler.parser.tree.NodeType;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Parses declaration.
+ *
+ * <pre>{@code
+ * <declaration> ::= {<declaration-specifier>}+ {<init-declarator>}* ;
+ * }</pre>
+ */
 public final class Declaration {
 
-    // <declaration> ::= {<declaration-specifier>}+ {<init-declarator>}* ;
 
     public static Node parse(Parser parser) {
 
-        List<Node> declarationSpecifiers = new ArrayList<>();
-        Node declarationSpecifier;
-        while ((declarationSpecifier = DeclarationSpecifier.parse(parser)) != null) {
-            declarationSpecifiers.add(declarationSpecifier);
-        }
+        // One or more
+        List<Node> dss = new ArrayList<>();
+        Node ds = DeclarationSpecifier.parse(parser);
+        if (ds != null) dss.add(ds);
+        while ((ds = DeclarationSpecifier.parse(parser)) != null) dss.add(ds);
 
-        if (declarationSpecifiers.isEmpty()) {
+        // If none then this is not it
+        if (dss.isEmpty()) return null;
+
+        // Zero or more
+        List<Node> ids = new ArrayList<>();
+        Node id;
+        while ((id = InitDeclarator.parse(parser)) != null) ids.add(id);
+
+        // Expect semicolon at the end
+        if (!parser.match(TokenType.SEMICOLON)) {
+            Token actual = parser.peek();
+            parser.addError(new ParserException(
+                    "Expected ';' at the end of the declaration.",
+                    actual.getLine(),
+                    actual.getColumn(),
+                    actual.getLexeme(),
+                    "';'",
+                    "Ensure that the declaration ends with a semicolon, e.g., 'int x;'."
+            ));
+            parser.synchronize();
             return null;
         }
 
-        Node initDeclarator;
-        while ((initDeclarator = InitDeclarator.parse(parser)) != null) {
-            declarationSpecifiers.add(initDeclarator);
-        }
+        // Combine all
+        dss.addAll(ids);
 
-        if (parser.match(TokenType.SEMICOLON)) {
-            Node result = new Node(
-                    NodeType.DECLARATION,
-                    declarationSpecifiers.getFirst().getLine(),
-                    declarationSpecifiers.getFirst().getColumn()
-            );
-            result.setChildren(declarationSpecifiers);
-            return result;
-        }
-
-        return null;
+        // Make ast node
+        Node result = new Node(NodeType.DECLARATION, dss.get(0).getLine(), dss.get(0).getColumn());
+        result.setChildren(dss);
+        return result;
     }
 
 }
--- src/main/resources/main.c
+++ src/main/resources/main.c
@@ -1,5 +1,6 @@
 int main() {
 
+    int y;
     int x = 2;
 
     if (2) {
